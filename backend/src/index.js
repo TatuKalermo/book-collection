7252931d8b448a052b3edb50e7f42f87975925a0
TatuKalermo <tatu.prayer@gmail.com>
2023-03-05 20:56:23 +0200

    Add babel packages and index.js

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
