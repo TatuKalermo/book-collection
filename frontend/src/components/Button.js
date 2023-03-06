ede508a76887d6a68a612e4e69e1fccaf40511a7
TatuKalermo <tatu.prayer@gmail.com>
2023-03-06 18:42:25 +0200

    Add id and children props for button

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SimpleButton = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+const SimpleButton = ({ id, children, onClick, disabled }) => {
+  return (
+    <button id={id} type="submit" disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 SimpleButton.propTypes = {
+  id: PropTypes.string.isRequired,
   children: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+SimpleButton.defaultProps = {
+  disabled: false,
 };
 
 export default SimpleButton;
