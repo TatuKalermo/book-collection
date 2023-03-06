e0d88b432843b29470497d005da95579991fdd35
TatuKalermo <tatu.prayer@gmail.com>
2023-03-06 18:43:13 +0200

    Add proptypes for textinput and wrap input inside label

--- frontend/src/components/TextInput.js
+++ frontend/src/components/TextInput.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextInput = ({ label, value, onChange, textArea }) => {
+const TextInput = ({ label, name, value, onChange, textArea }) => {
   return (
     <div className="form-group">
-      <label>{label}</label>
-      {textArea ? (
-        <textArea className="form-textarea" value={value} onChange={onChange} />
-      ) : (
-        <input
-          className="form-input"
-          type="text"
-          value={value}
-          onChange={onChange}
-          required
-        />
-      )}
+      <label>
+        {label}
+        {textArea ? (
+          <textarea
+            className="form-textarea"
+            name={name}
+            value={value}
+            onChange={onChange}
+          />
+        ) : (
+          <input
+            className="form-input"
+            name={name}
+            type="text"
+            value={value}
+            onChange={onChange}
+            required
+          />
+        )}
+      </label>
     </div>
   );
 };
 
 TextInput.propTypes = {
   label: PropTypes.string.isRequired,
+  name: PropTypes.string,
   value: PropTypes.string,
-  textArea: PropTypes.bool.isRequired,
+  textArea: PropTypes.bool,
   onChange: PropTypes.func.isRequired,
+};
+
+TextInput.defaultProps = {
+  name: '',
+  value: '',
+  textArea: false,
 };
 
 export default TextInput;
